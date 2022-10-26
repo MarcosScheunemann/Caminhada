fe364e5fe9dc3c420b308bc812d386c47e014001
Marcos Scheunemann <marcos_verg@hotmail.com>
2022-10-26 11:25:10 -0300

    new file:   C#/explorando_a_linguagem/aprendendo_CSharp/.vs/ProjectEvaluation/aprendendo_csharp.metadata.v5.1 	new file:   C#/explorando_a_linguagem/aprendendo_CSharp/.vs/ProjectEvaluation/aprendendo_csharp.projects.v5.1 	new file:   C#/explorando_a_linguagem/aprendendo_CSharp/.vs/aprendendo_CSharp/DesignTimeBuild/.dtbcache.v2 	deleted:    C#/explorando_a_linguagem/aprendendo_CSharp/.vs/aprendendo_CSharp/FileContentIndex/3f79504d-0b3c-43f7-8f66-c4a942517f64.vsidx 	new file:   C#/explorando_a_linguagem/aprendendo_CSharp/.vs/aprendendo_CSharp/FileContentIndex/9d00d11d-5e95-40a7-9d0a-fb29350cae5c.vsidx 	new file:   C#/explorando_a_linguagem/aprendendo_CSharp/.vs/aprendendo_CSharp/v17/.futdcache.v2 	modified:   C#/explorando_a_linguagem/aprendendo_CSharp/.vs/aprendendo_CSharp/v17/.suo 	modified:   C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/Program.cs 	modified:   C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/bin/Debug/net6.0/1-Olamundo.dll 	modified:   C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/bin/Debug/net6.0/1-Olamundo.pdb 	modified:   C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/obj/Debug/net6.0/1-Olamundo.dll 	modified:   C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/obj/Debug/net6.0/1-Olamundo.pdb 	new file:   projeto1/readme.tx

--- C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/Program.cs
+++ C#/explorando_a_linguagem/aprendendo_CSharp/1-Olamundo/Program.cs
@@ -2,9 +2,39 @@
 
 class Programa
 {
+
     static void Main(string[] args)
     {
         Console.WriteLine("Olá, mundo");
+        int idade;
+        idade = 38;
+        idade = idade - 2 * 9;
+        double carteira = 53.696;
+        char letra = 'a';
+        string frase = "é assim que se declara uma string";
+
+        Console.WriteLine(carteira + letra + frase);
+
+        if (idade >=18 || letra == 'a' && idade == 38)
+        {
+            Console.WriteLine("Maiore");
+        }
+        else
+        {
+            Console.WriteLine("Menore");
+        }
+
+
+        int mes = 1;
+        while (mes <= 12)
+        {
+            Console.WriteLine(mes);
+            mes++;
+        }
+
+        Console.WriteLine(idade);
+        Console.WriteLine("tecle enter para fechar...");
+        Console.ReadLine();
     }
 }
 
